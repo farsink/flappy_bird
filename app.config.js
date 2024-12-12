17c55937d82dec5978f053b8159e402372ef29fc
farsink <farsin495@gmail.com>
2024-12-12 15:25:51 +0530

    dist added

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default {
+  expo: {
+    name: "flappy_bird",
+    slug: "flappy_bird",
+    platforms: ["ios", "android", "web"],
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+  },
+};
